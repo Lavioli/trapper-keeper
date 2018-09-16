ce769ebccd1092d80e645a2686f85da3ba8c10d5
Panda Geeks <pandageeks@lavies-macbook.attlocal.net>
2018-09-16 14:20:13 -0500

    finish private file upload

--- src/Application.js
+++ src/Application.js
@@ -1,10 +1,39 @@
-import React, { Component } from 'react';
-import './Application.css';
+import React, { Component } from "react";
+import "./Application.css";
+
+import { withAuthenticator } from "aws-amplify-react";
+import { Storage } from "aws-amplify";
+
+Storage.configure({ level: "private" });
+
+class S3Image extends Component {
+  state = { src: null };
+
+  async componentDidMount() {
+    const { s3key } = this.props;
+    const src = await Storage.get(s3key);
+    this.setState({ src });
+  }
+  render() {
+    const { src } = this.state;
+    if (!src) return null;
+    return (
+      <article>
+        <img src={src} alt="img" />
+      </article>
+    );
+  }
+}
 
 class Application extends Component {
   state = {
     files: []
   };
+
+  async componentDidMount() {
+    const files = await Storage.list("");
+    this.setState({ files });
+  }
 
   handleSubmit = event => {
     event.preventDefault();
@@ -12,25 +41,27 @@ class Application extends Component {
     const file = this.fileInput.files[0];
     const { name } = file;
 
-    console.log(file, name);
+    Storage.put(name, file).then(response => {
+      console.log("Storage.put", { response });
+      this.setState({ files: [...this.state.files, response] });
+    });
   };
 
   render() {
     return (
       <div className="Application">
         <form className="NewItem" onSubmit={this.handleSubmit}>
-          <input
-            type="file"
-            ref={input => this.fileInput = input}
-          />
+          <input type="file" ref={input => (this.fileInput = input)} />
           <input className="full-width" type="submit" />
         </form>
         <section className="Application-images">
-
+          {this.state.files.map((file, i) => (
+            <S3Image s3key={file.key} key={`img-${i}`} />
+          ))}
         </section>
       </div>
     );
   }
 }
 
-export default Application;
+export default withAuthenticator(Application);
